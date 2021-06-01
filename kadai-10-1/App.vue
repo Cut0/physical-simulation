456804fbdb64ab29351034751c0e9192e055040b
CutRay <rei.kato@mixi.co.jp>
2021-06-01 10:00:59 +0900

    授業内課題追加

---
+++ kadai-10-1/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-app>
+    <v-app-bar dark fixed color="#EF4455">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>教育システムデザイン第10回</v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-card class="mx-auto" max-width="720">
+          <v-container>
+            <p>4619023：加藤零</p>
+            <canvas ref="myCanvas" width="300" height="300"></canvas>
+          </v-container>
+        </v-card>
+      </v-container>
+      <v-container>
+        <v-card class="mx-auto" max-width="720">
+          <v-container>
+            <p class="headline">ボール情報</p>
+            <v-container>
+              <v-row>
+                <v-col align="center">
+                  <v-progress-circular
+                    :rotate="-90"
+                    :size="70"
+                    :width="8"
+                    :value="(state.time % 60) * (100 / 60)"
+                    color="#EF4455"
+                    >{{ state.time.toFixed(1) }}
+                  </v-progress-circular>
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-container>
+              <v-data-table
+                dense
+                :headers="headers"
+                :items="table"
+                item-key="title"
+                class="elevation-1"
+              ></v-data-table>
+            </v-container>
+            <v-container>
+              <v-btn
+                dark
+                rounded
+                block
+                color="#EF4455"
+                @click="state.switchMoving"
+                >{{ state.isMoving ? '停止' : '再開' }}</v-btn
+              >
+            </v-container>
+          </v-container>
+        </v-card>
+      </v-container>
+      <v-container>
+        <v-card class="mx-auto" max-width="720">
+          <v-container>
+            <p class="headline">実装内容・工夫点</p>
+            <p>
+              今回の課題ではUIを整えるためにvuetifyを利用した．また，JS単体だと補完機能が乏しく安全性も低いためTypeScriptを利用してJSにトランスパイルしwebpackでbundleすることで課題に取り組んだ．また，vue2にcomposition-apiを入れることで開発効率を向上させた．vuetifyが最新のvue3に対応していないためvue2を利用している．webpackは最新の5系を取り入れている.
+            </p>
+          </v-container>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script lang="ts">
+import {
+  ref,
+  defineComponent,
+  onMounted,
+  reactive,
+  watch,
+  computed,
+} from '@vue/composition-api';
+import AnimateComponent from './modules/animate';
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      time: 0.0,
+      position: { y: '0.0' },
+      vector: { y: '0.0' },
+      switchMoving: () => {},
+      isMoving: false,
+    });
+    const myCanvas = ref<null | HTMLCanvasElement>(null);
+
+    onMounted(() => {
+      const canvas = myCanvas.value!;
+      const animateComponent = AnimateComponent(canvas);
+      animateComponent.drawCanvas();
+      animateComponent.animate();
+      watch(animateComponent.isMoving, (val) => (state.isMoving = val), {
+        immediate: true,
+      });
+      watch(animateComponent.box.value, (val) => {
+        state.position.y = val.y.toFixed(1);
+        state.vector.y = val.vy.toFixed(1);
+      });
+
+      state.switchMoving = animateComponent.switchMoving;
+      setInterval(() => {
+        if (animateComponent.isMoving.value) state.time += 0.1;
+      }, 100);
+    });
+
+    const headers = computed(() => [
+      { text: 'パラメータ', value: 'title' },
+      { text: 'y', value: 'y' },
+      { text: 'vy', value: 'vy' },
+    ]);
+
+    const table = computed(() => [
+      { title: '位置', y: state.position.y },
+      { title: '速度', vy: state.vector.y },
+    ]);
+
+    return { state, myCanvas, headers, table };
+  },
+});
+</script>
+<style>
+.v-main {
+  padding-top: 64px !important;
+}
+</style>
+<style scoped></style>
---
+++ kadai-10-1/modules/animate.ts
@@ -0,0 +1,101 @@
+import { reactive, computed } from '@vue/composition-api';
+
+type Box = {
+  k: number; //ばね定数
+  side: number; //一辺の長さ
+  m: number; //重さ
+  y: number; //位置
+  vy: number; //速度
+};
+
+type PositionList = number[];
+
+const MAX_LENGTH = 50000;
+const step = 0.1;
+
+export default (canvas: HTMLCanvasElement) => {
+  const ctx = canvas.getContext('2d')!;
+  ctx.translate(0, canvas.height / 2);
+  ctx.scale(1, -1);
+
+  const state = reactive<{
+    box: Box;
+    positions: PositionList;
+    time: number;
+    isMoving: boolean;
+  }>({
+    box: {
+      k: 0.5,
+      side: 25,
+      m: 2,
+      y: 0,
+      vy: 20,
+    },
+    positions: [0],
+    time: 0,
+    isMoving: true,
+  });
+
+  const time = computed(() => state.time);
+  const isMoving = computed(() => state.isMoving);
+  const box = computed(() => state.box);
+
+  const drawCanvas = (): void => {
+    ctx.clearRect(0, -canvas.height / 2, canvas.width, canvas.height);
+
+    // 枠線の描画
+    ctx.strokeRect(0, -canvas.height / 2, canvas.width, canvas.height);
+
+    // 軸の描画
+    ctx.strokeStyle = 'gray';
+    ctx.beginPath();
+    ctx.moveTo(state.box.side, 0);
+    ctx.lineTo(canvas.width, 0);
+    ctx.moveTo(state.box.side, -canvas.height / 2);
+    ctx.lineTo(state.box.side, canvas.height);
+    ctx.stroke();
+
+    // 物体の描画
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(
+      0,
+      state.box.y - state.box.side / 2,
+      state.box.side,
+      state.box.side,
+    );
+  };
+
+  const animate = () => {
+    drawCanvas();
+    state.box.vy += -(state.box.k / state.box.m) * state.box.y * step;
+    state.box.y += state.box.vy * step;
+
+    state.time += step;
+    if (state.positions.length < MAX_LENGTH) state.positions.push(state.box.y);
+
+    //軌跡の描画
+    ctx.beginPath();
+    ctx.moveTo(state.box.side, 0);
+    state.positions.forEach((pos, i) => {
+      ctx.lineTo(state.box.side + i, pos);
+    });
+    ctx.stroke();
+
+    if (state.time < 10000 && state.isMoving) requestAnimationFrame(animate);
+  };
+
+  const switchMoving = () => {
+    state.isMoving = !state.isMoving;
+    if (state.isMoving) animate();
+  };
+
+  return {
+    time,
+    isMoving,
+    box,
+    switchMoving,
+    drawCanvas,
+    animate,
+    stop,
+  };
+};
